109614a601ac1be72991fd9ca5ec219b13a7430a
Christopher Santero <chris@solveitsimply.com>
2015-05-30 10:26:00 -0400

    add acceptance test

---
+++ app/acceptance-tests/sinon.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-sinon/acceptance-tests/sinon';
---
+++ tests/acceptance/sinon-test.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from 'dummy/tests/helpers/start-app';
+import sinon from 'sinon';
+
+var application;
+
+module('Acceptance | sinon', {
+  beforeEach: function() {
+    application = startApp();
+  },
+
+  afterEach: function() {
+    Ember.run(application, 'destroy');
+  }
+});
+
+test('sinon is available as an import and works', function (assert) {
+  var foo = sinon.spy();
+
+  foo(3);
+  assert.equal(foo.callCount, 1, 'Spy was called the right number of times.');
+  assert.equal(foo.firstCall.args[0], 3, 'Spy was called with the right argument.');
+});
