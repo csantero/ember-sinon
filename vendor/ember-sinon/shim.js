ff5ea6655a0f68229fb8e7699db1a0d84fa5bf93
Chris Santero <chris@solveitsimply.com>
2015-10-09 22:14:36 -0400

    remove qunit code

--- vendor/ember-sinon/shim.js
+++ vendor/ember-sinon/shim.js
@@ -1,38 +1,7 @@
-/* globals define, sinon, QUnit */
+/* globals define, sinon */
 
 define('sinon', [], function() {
   "use strict";
-
-  if (typeof QUnit !== 'undefined') {
-    sinon.expectation.fail = sinon.assert.fail = function (msg) {
-      QUnit.ok(false, msg);
-    };
-
-    sinon.assert.pass = function (assertion) {
-      QUnit.ok(true, assertion);
-    };
-
-    sinon.config = {
-      injectIntoThis: true,
-      injectInto: null,
-      properties: ["spy", "stub", "mock", "clock", "sandbox"],
-      useFakeTimers: false,
-      useFakeServer: false
-    };
-
-    (function (global) {
-      var qTest = QUnit.test;
-
-      QUnit.test = global.test = function (testName, expected, callback, async) {
-        if (arguments.length === 2) {
-          callback = expected;
-          expected = null;
-        }
-
-        return qTest(testName, expected, sinon.test(callback), async);
-      };
-    }(this));
-  }
 
   return {
     'default': sinon
