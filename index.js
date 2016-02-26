52f98a82d978e7604e0346ad9f6376d55bf44626
Dan Freeman <dfreeman@salsify.com>
2016-02-26 15:22:27 -0500

    Pull sinon from npm instead of Bower

--- index.js
+++ index.js
@@ -4,12 +4,17 @@
 module.exports = {
   name: 'ember-sinon',
 
-  included: function (app) {
-    if (app.tests) {
-      app.import('bower_components/sinonjs/sinon.js', {
-        type: 'test'
-      });
+  options: {
+    nodeAssets: {
+      'sinon': {
+        import: [{ path: 'pkg/sinon.js', type: 'test' }]
+      }
+    }
+  },
 
+  included: function (app) {
+    this._super.included.apply(this, arguments);
+    if (app.tests) {
       app.import('vendor/ember-sinon/shim.js', {
         type: 'test',
         exports: { 'sinon': ['default'] }
