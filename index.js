a5b5926140c0728987057c97134c51810e6447bd
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-12-05 20:38:31 +0100

    Implement PR feedback

--- index.js
+++ index.js
@@ -8,18 +8,23 @@ var MergeTrees = require('broccoli-merge-trees');
 module.exports = {
   name: 'ember-sinon',
 
-  included: function(app) {
+  included: function (app) {
     this._super.included.apply(this, arguments);
 
-    while (typeof app.import !== 'function' && app.app) {
-      app = app.app;
+    var importContext;
+    if (this.import) {
+      // support for ember-cli >= 2.7
+      importContext = this;
+    } else {
+      // addon support for ember-cli < 2.7
+      importContext = this._findHostForLegacyEmberCLI();
     }
 
-    app.import('vendor/sinon/sinon.js', { type: 'test' });
-    app.import('vendor/shims/sinon.js', { type: 'test' });
+    importContext.import('vendor/sinon/sinon.js', { type: 'test' });
+    importContext.import('vendor/shims/sinon.js', { type: 'test' });
   },
 
-  treeForVendor: function(tree) {
+  treeForVendor: function (tree) {
     var sinonPath = path.dirname(require.resolve('sinon/pkg/sinon'));
     var sinonTree = new Funnel(sinonPath, {
       files: ['sinon.js'],
@@ -31,5 +36,20 @@ module.exports = {
     return new MergeTrees(trees, {
       annotation: 'ember-sinon: treeForVendor'
     });
+  },
+
+  // included from https://git.io/v6F7n
+  // not needed for ember-cli > 2.7
+  _findHostForLegacyEmberCLI: function() {
+    var current = this;
+    var app;
+
+    // Keep iterating upward until we don't have a grandparent.
+    // Has to do this grandparent check because at some point we hit the project.
+    do {
+      app = current.app || app;
+    } while (current.parent.parent && (current = current.parent));
+
+    return app;
   }
 };
