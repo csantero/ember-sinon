a3732e8741a7bdfa2061776ad6d83a609f7d0664
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-12-05 20:27:30 +0100

    Replace "ember-cli-node-assets" with treeForVendor() hook

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
   name: 'ember-sinon',
-
-  options: {
-    nodeAssets: {
-      'sinon': {
-        import: [{ path: 'pkg/sinon.js', type: 'test' }]
-      }
-    }
-  },
 
   included: function(app) {
     this._super.included.apply(this, arguments);
@@ -19,6 +15,21 @@ module.exports = {
       app = app.app;
     }
 
+    app.import('vendor/sinon/sinon.js', { type: 'test' });
     app.import('vendor/shims/sinon.js', { type: 'test' });
+  },
+
+  treeForVendor: function(tree) {
+    var sinonPath = path.dirname(require.resolve('sinon/pkg/sinon'));
+    var sinonTree = new Funnel(sinonPath, {
+      files: ['sinon.js'],
+      destDir: '/sinon',
+    });
+
+    var trees = [tree, sinonTree];
+
+    return new MergeTrees(trees, {
+      annotation: 'ember-sinon: treeForVendor'
+    });
   }
 };
